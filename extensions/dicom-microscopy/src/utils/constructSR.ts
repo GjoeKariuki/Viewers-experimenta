de35a5d662953ee2d95bc230f669aa9c052ec054
GjoeKariuki <githaigageorge96@gmail.com>
2026-06-05 10:06:36 +0300

    stable 90

--- extensions/dicom-microscopy/src/utils/constructSR.ts
+++ extensions/dicom-microscopy/src/utils/constructSR.ts
@@ -175,7 +175,7 @@ export default function constructSR(metadata, { SeriesDescription, SeriesNumber 
     MediaStorageSOPClassUID: dataset.sopClassUID,
     MediaStorageSOPInstanceUID: dataset.sopInstanceUID,
     TransferSyntaxUID: {
-      Value: ['1.2.840.10008.1.2.4.91'], // Explicit VR Little Endian
+      Value: ['1.2.840.10008.1.2.4.90'], // Explicit VR Little Endian
       vr: 'UI',
     },
     ImplementationClassUID: {
